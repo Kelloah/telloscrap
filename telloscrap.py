9d85c70e23394a1057ff426ed19a92a8a2c96aab
curosim <75795978+curosim@users.noreply.github.com>
2022-04-13 15:26:50 +0200

    Update telloscrap.py

--- telloscrap.py
+++ telloscrap.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import sys
+import argparse
 
 headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.7113.93 Safari/537.36',
@@ -10,13 +12,19 @@ headers = {
 def get_user_info(username):
 	"""	Gets informations about a username (We need the User-ID)
 	"""
-	response = requests.get('https://api.tellonym.me/profiles/name/{0}'.format(username), headers=headers)
-	data = json.loads(response.text)
-	if 'err' in data:
+	try:
+		response = requests.get('https://api.tellonym.me/profiles/name/{0}'.format(username), headers=headers)
+		data = json.loads(response.text)
+	except Exception as e:
+		print("[!] Failed to connect to the Tellonym API (https://api.tellonym.me)")
 		return False, None
 	else:
-		del data['answers']
-		return True, data
+		if 'err' in data:
+			print("[!] User '{0}' doenst seem to exist".format(username))
+			return False, None
+		else:
+			del data['answers']
+			return True, data
 
 def get_tells_of_user(user_id):
 	""" Iterates trough all tells/answers of the user and collects them in a list of dictionaries.
@@ -46,37 +54,61 @@ def banner():
 	print("    | |  __/ | | (_) \__ \ (__| | | (_| | |_) |")
 	print("    |_|\___|_|_|\___/|___/\___|_|  \__,_| .__/ ")
 	print("                                        | |    ")
-	print("                                        |_|    \n")
+	print("    Version: 1.0 - Author: @curosim     |_|    \n")
 
-def close():
+def abort():
 	print("[*] Aborting...")
 	exit()
+
 
 def main():
 	banner()
 
-	username = input('[*] Please enter Tellonym username: ')
+	parser = argparse.ArgumentParser(
+		description='Crawler for Tellonym.me Users Questions and Answers.',
+		formatter_class=argparse.RawDescriptionHelpFormatter
+	)
+	parser.add_argument(
+		"user",
+		help="Username (tellonym.me/USERNAME)"
+	)
+	parser.add_argument(
+		"-m", "--metadata",
+		type=str,
+		choices=['full','basic'],
+		default='basic',
+		help="Choose which (default: %(default)s)"
+	)
+	args = parser.parse_args()
 
-	data = {}
-	success, info = get_user_info(username)
+
+	print("[*] Starting to crawl data for user '{0}'".format(args.user))
+
+	# Check if user exists
+	success, info = get_user_info(args.user)
 	if success == False:
-		print("[!] User '{0}' doenst seem to exist".format(username))
-		close()
+		abort()
+	else:
+		print("[*] User '{0}' exists!".format(args.user))
 
-	print("[*] Starting to collect tells of user '{0}'".format(username))
+	print("[*] Collecting questions and answers")
 
+	# Collect all questsions and answers
+	data = {}
 	data['user_info'] = info
 	data['answers'] = get_tells_of_user(user_id=data['user_info']['id'])
 
+	# Display crawling summary
 	print("[*] FINISHED!")
 	print("    Total tells: {0}".format(data['user_info']['tellCount']))
 	print("    Crawled tells: {0}".format(len(data['answers'])))
 	
+	# Write results to file
 	filename = '{0}.json'.format(data['user_info']['username'])
 	with open(filename, 'w') as f:
 		f.write(json.dumps(data, indent=4, sort_keys=True))
 
-	print("[*] All written to file '{0}'".format(filename))
+	print("[*] Results written to file '{0}'".format(filename))
 
 
 if __name__ == '__main__':
