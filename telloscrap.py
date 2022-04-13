f7e20748a1d0d5f140bb1cfd5e705faf209372d8
curosim <75795978+curosim@users.noreply.github.com>
2022-04-14 01:46:38 +0200

    Update telloscrap.py

--- telloscrap.py
+++ telloscrap.py
@@ -4,8 +4,8 @@ import sys
 import argparse
 
 headers = {
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.7113.93 Safari/537.36',
-	'tellonym-client': 'web:0.51.1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0)',
+	'tellonym-client': 'web:0.63.5',
 	'content-type': 'application/json;charset=utf-8'
 }
 
@@ -35,9 +35,10 @@ def get_tells_of_user(user_id):
 		params = {'limit':'100','pos':str(position)}
 		response = requests.get('https://api.tellonym.me/answers/{0}'.format(user_id), headers=headers, params=params)
 		new_tells = json.loads(response.text)['answers']
-		
+
 		for tell in new_tells: tells.append(tell)
-		print("  > Got another {0} from API - (Total: {1})".format(len(new_tells), len(tells)))
+		if len(new_tells) > 0:
+			print("  > Got another {0} from API - (Total collected: {1})".format(len(new_tells), len(tells)))
 		
 		if len(new_tells) == 100:
 			position = position + 100
@@ -45,6 +46,15 @@ def get_tells_of_user(user_id):
 			print("  > Last page reached")
 			break;
 	return tells
+
+def remove_unnecessary_data(data):
+	# I know this is not particularly nice, but I needed a fast solution :P
+	keys_to_keep = ['tell', 'answer', 'createdAt']
+	data_new = {'user_info':data['user_info'], 'answers':[]}
+	for answer in data['answers']:
+		new_answer = {'tell':answer['tell'], 'answer':answer['answer'], 'createdAt':answer['createdAt']}
+		data_new['answers'].append(new_answer)
+	return data_new
 
 def banner():
 	print("  _______   _ _                                ")
@@ -56,40 +66,40 @@ def banner():
 	print("                                        | |    ")
 	print("    Version: 1.0 - Author: @curosim     |_|    \n")
 
+def help(args):
+	print("    Description:")
+	print("    Telloscrap is a crawler for Tellonym.me Users.")
+	print("    It downloads User Info, Questions and Answers and saves it to a file.\n")
+	print("    Usage: {0} USERNAME [--full]".format(args[0]))
+	print("    Use --full to save metadata too (Default: False).")
+
+
 def abort():
 	print("[*] Aborting...")
 	exit()
 
 
-def main():
+def main(args):
 	banner()
 
-	parser = argparse.ArgumentParser(
-		description='Crawler for Tellonym.me Users Questions and Answers.',
-		formatter_class=argparse.RawDescriptionHelpFormatter
-	)
-	parser.add_argument(
-		"user",
-		help="Username (tellonym.me/USERNAME)"
-	)
-	parser.add_argument(
-		"-m", "--metadata",
-		type=str,
-		choices=['full','basic'],
-		default='basic',
-		help="Choose which (default: %(default)s)"
-	)
-	args = parser.parse_args()
+	# Check if the minimal amount of arguments got passed (pos 0 is always the script name)
+	if len(args) < 2:
+		help(args)
+		exit()
+	
+	# Check if the user requested help
+	if args[1] == '-h' or args[1] == '--help':
+		help(args)
+		exit()
 
-
-	print("[*] Starting to crawl data for user '{0}'".format(args.user))
+	print("[*] Starting to crawl data for user '{0}'".format(args[1]))
 
 	# Check if user exists
-	success, info = get_user_info(args.user)
+	success, info = get_user_info(args[1])
 	if success == False:
 		abort()
 	else:
-		print("[*] User '{0}' exists!".format(args.user))
+		print("[*] User '{0}' exists!".format(args[1]))
 
 	print("[*] Collecting questions and answers")
 
@@ -102,7 +112,20 @@ def main():
 	print("[*] FINISHED!")
 	print("    Total tells: {0}".format(data['user_info']['tellCount']))
 	print("    Crawled tells: {0}".format(len(data['answers'])))
+	if data['user_info']['tellCount'] > len(data['answers']):
+		if len(data['answers']) == 0:
+			print("    Note: Although there were tells once, we cant see them anymore :(")
+		else:
+			print("    Note: It seems not all Tells can be seen anymore.")
+
 	
+	# Clean the JSON of data we dont need
+	keep_metadata = False
+	if len(args) == 3:
+		if args[2] == '--full':
+			keep_metadata = True
+	if keep_metadata == False: data = remove_unnecessary_data(data)
+
 	# Write results to file
 	filename = '{0}.json'.format(data['user_info']['username'])
 	with open(filename, 'w') as f:
@@ -112,4 +135,8 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+	try:
+		main(sys.argv)
+	except KeyboardInterrupt:
+		print("[*] Execution stopped by keyboard!")
+		abort()
